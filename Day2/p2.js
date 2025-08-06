d491780f9c7a912b31193f5d7dbbf30fc5bc11ff
PREET-BHARDWAJ <146235081+PREET-BHARDWAJ@users.noreply.github.com>
2025-08-06 13:21:31 +0530

    Delete Day2 directory

--- Day2/p2.js
+++
@@ -1,9 +0,0 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    res.statusCode=200;
-    res.setHeader('content-Type','text/plain');
-    res.end('Hello World');
-})
-server.listen(9000,()=>{
-    console.log('server running on port 9000');
-})
